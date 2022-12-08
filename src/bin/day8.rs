44ffb23214048b996480ea40c391f352b4b9bbd8
Sebastian Holmin <sebastian.holmin@bluetest.se>
2022-12-08 21:04:54 +0100

    Clean up part 1 of day 8

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -2,83 +2,68 @@ use itertools::Itertools;
 
 fn main() {
     let input = std::fs::read_to_string("input/day8.txt").unwrap();
-    println!("Answer: {}", solve_2(&input));
+    println!("Answer 1: {}", solve(&input));
+    println!("Answer 2: {}", solve_2(&input));
 }
 
-fn solve(input: &str) -> i32 {
-    let cols = input.lines().next().unwrap().len();
-    let rows = input.lines().collect::<Vec<&str>>().len();
-
-    let height = input
+fn get_heights(input: &str) -> Vec<Vec<i32>> {
+    input
         .lines()
         .map(|line| {
             line.chars()
                 .map(|chr| chr.to_digit(10).unwrap() as i32)
                 .collect_vec()
         })
-        .collect_vec();
+        .collect_vec()
+}
 
-    let mut visible = (0..rows)
-        .map(|_| (0..cols).map(|_| 0).collect_vec())
-        .collect_vec();
+fn solve(input: &str) -> i32 {
+    let num_cols = input.lines().next().unwrap().len();
+    let num_rows = input.lines().count();
+
+    let height = get_heights(input);
+
+    let mut visible = vec![vec![0; num_cols]; num_rows];
 
     for (row, vis_row) in height.iter().zip(visible.iter_mut()) {
-        let mut max = -1;
-        for (height, vis) in row.iter().zip(vis_row.iter_mut()) {
-            if *height > max {
-                max = *height;
-                *vis = 1;
-            }
-        }
-        let mut max = -1;
-        for (height, vis) in row.iter().zip(vis_row.iter_mut()).rev() {
-            if *height > max {
-                max = *height;
-                *vis = 1;
-            }
-        }
+        fill_visible_line(row.iter(), vis_row.iter_mut());
+        fill_visible_line(row.iter().rev(), vis_row.iter_mut().rev());
     }
 
-    for col in 0..cols {
-        let mut max = -1;
-        for (height, vis) in height
-            .iter()
-            .map(|row| row.get(col).unwrap())
-            .zip(visible.iter_mut().map(|row| row.get_mut(col).unwrap()))
-        {
-            if *height > max {
-                max = *height;
-                *vis = 1;
-            }
-        }
-        let mut max = -1;
-        for (height, vis) in height
-            .iter()
-            .map(|row| row.get(col).unwrap())
-            .zip(visible.iter_mut().map(|row| row.get_mut(col).unwrap()))
-            .rev()
-        {
-            if *height > max {
-                max = *height;
-                *vis = 1;
-            }
-        }
+    for col in 0..num_cols {
+        fill_visible_line(
+            height.iter().map(|row| row.get(col).unwrap()),
+            visible.iter_mut().map(|row| row.get_mut(col).unwrap()),
+        );
+        fill_visible_line(
+            height.iter().map(|row| row.get(col).unwrap()).rev(),
+            visible
+                .iter_mut()
+                .map(|row| row.get_mut(col).unwrap())
+                .rev(),
+        );
     }
     visible.iter().map(|row| row.iter().sum::<i32>()).sum()
+}
+
+fn fill_visible_line<'a>(
+    heights: impl Iterator<Item = &'a i32>,
+    visible_in_line: impl Iterator<Item = &'a mut i32>,
+) {
+    let mut max = -1;
+    for (height, vis) in heights.zip(visible_in_line) {
+        if *height > max {
+            max = *height;
+            *vis = 1;
+        }
+    }
 }
 
 fn solve_2(input: &str) -> usize {
     let num_cols = input.lines().next().unwrap().len();
     let num_rows = input.lines().count();
 
-    let height = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|chr| chr.to_digit(10).unwrap() as i32)
-                .collect_vec()
-        })
-        .collect_vec();
+    let height = get_heights(input);
 
     let mut max_scenic_score = 0;
     for i_row in 1..num_rows - 1 {
@@ -93,7 +78,8 @@ fn solve_2(input: &str) -> usize {
 
             let vis_down = distance_to_tree_of_same_height(col.iter().skip(i_row));
 
-            let vis_up = distance_to_tree_of_same_height(col.iter().rev().skip(num_rows - i_row - 1));
+            let vis_up =
+                distance_to_tree_of_same_height(col.iter().rev().skip(num_rows - i_row - 1));
 
             let scenic_score = vis_to_right * vis_to_left * vis_up * vis_down;
             if scenic_score > max_scenic_score {
@@ -105,11 +91,12 @@ fn solve_2(input: &str) -> usize {
     max_scenic_score
 }
 
-fn distance_to_tree_of_same_height<'a>(mut view: impl Iterator<Item = &'a i32>) -> usize {
-    let max_height = view.next().unwrap();
-    if let Some((i, _)) = view
+fn distance_to_tree_of_same_height<'a>(mut height_view: impl Iterator<Item = &'a i32>) -> usize {
+    let max_height = height_view.next().unwrap();
+    if let Some((i, _)) = height_view
         .enumerate()
-        .find_or_last(|(_, look_height)| *look_height >= max_height) {
+        .find_or_last(|(_, look_height)| *look_height >= max_height)
+    {
         i + 1
     } else {
         0
