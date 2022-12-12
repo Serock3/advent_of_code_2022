c355056f32b8c3c14ded760d06183cbf0015a8b5
Sebastian Holmin <sebastian.holmin@bluetest.se>
2022-12-12 10:28:39 +0100

    template

---
+++ src/bin/template.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let input = std::fs::read_to_string("input/day??.txt").unwrap();
+    println!("Answer 1: {}", solve(&input));
+    // println!("Answer 2: {}", solve_2(&input));
+}
+
+fn solve(input: &str) -> usize {
+    todo!()
+}
+
+#[test]
+fn test_example() {
+    let input = "";
+    assert_eq!(solve(input), todo!())
+}
